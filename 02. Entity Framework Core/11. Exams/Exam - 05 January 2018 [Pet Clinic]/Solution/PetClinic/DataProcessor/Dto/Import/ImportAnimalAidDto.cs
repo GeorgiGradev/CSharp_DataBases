e22d01f2aa84522aaa3c3386af73e7f8874be30a
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:37:00 +0300

    Create ImportAnimalAidDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Import/ImportAnimalAidDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.DataProcessor.Dto.Import
+{
+    public class ImportAnimalAidDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        //UNIQUE
+        public string Name { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+    }
+}
