cfe16d27b0e1aeaf72e7acf45c98abbb87016e57
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:27 +0300

    Create Passport.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Models/Passport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.Models
+{
+    public class Passport
+    {
+        [Key]
+        [RegularExpression(@"^[A-Za-z]{7}[0-9]{3}$")]
+        public string SerialNumber  { get; set; }
+
+        [Required]
+        public virtual Animal Animal { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[+]359[0-9$]{9}$|^0[0-9]{9}$")]
+        public string OwnerPhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string OwnerName { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
