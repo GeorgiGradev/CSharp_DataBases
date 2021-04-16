4337095f9e73cf04e62d1b9d3b674d7724fe796f
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:37:15 +0300

    Create PetClinicContext.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Data/PetClinicContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetClinic.Models;
+
+namespace PetClinic.Data
+{
+
+    public class PetClinicContext : DbContext
+    {
+        public PetClinicContext()
+        {
+        }
+
+        public PetClinicContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<AnimalAid> AnimalAids { get; set; }
+        public DbSet<Passport> Passports { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
+        public DbSet<ProcedureAnimalAid> ProceduresAnimalAids { get; set; }
+        public DbSet<Vet> Vets { get; set; }
+        public object Where { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ProcedureAnimalAid>(entity =>
+            {
+                entity.HasKey(x => new
+                {
+                    x.AnimalAidId,
+                    x.ProcedureId
+                });
+            });
+        }
+    }
+}
