c744b6e4290e9ed8b1a6688f28ea1171bdcd5b50
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:34 +0300

    Create Animal.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Models/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PetClinic.Models
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            this.Procedures = new HashSet<Procedure>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Type { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Age { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Passport))]
+        public string PassportSerialNumber { get; set; }
+
+        [Required]
+        public virtual Passport Passport { get; set; }
+
+        public virtual ICollection<Procedure> Procedures { get; set; }
+    }
+}
