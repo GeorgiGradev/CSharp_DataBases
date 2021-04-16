16c7897dd2985d54bf050c2ed2e0fc8283f0d223
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:37:02 +0300

    Create ExportProcedureDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Export/ExportProcedureDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PetClinic.DataProcessor.Dto.Export
+{
+    [XmlType("Procedure")]
+    public class ExportProcedureDto
+    {
+        [XmlElement("Passport")]
+        public string PassportNumber { get; set; }
+
+        [XmlElement("OwnerNumber")]
+        public string OwnerNumber { get; set; }
+
+        [XmlElement("DateTime")]
+        public string ProcedureDate { get; set; }
+
+        [XmlArray("AnimalAids")]
+        public List<ExportAninalAidDto> AnimalAids { get; set; }
+
+        [XmlElement("TotalPrice")]
+        public decimal TotalPrice { get; set; }
+    }
+}
