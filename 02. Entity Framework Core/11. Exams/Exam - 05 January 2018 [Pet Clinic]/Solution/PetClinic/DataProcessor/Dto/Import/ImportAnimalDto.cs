187187ec9f671789e59a8c35660c049a560eb577
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:56 +0300

    Create ImportAnimalDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Import/ImportAnimalDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.DataProcessor.Dto.Import
+{
+    public class ImportAnimalDto
+    {
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Type { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Age { get; set; }
+
+        [Required]
+        public ImportAnimalPassportDto Passport { get; set; }
+    }
+}
+
