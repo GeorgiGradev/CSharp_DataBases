432c1bfe19d9dc611655a786f45566fdd438e89c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:20 +0300

    Create ProcedureAnimalAid.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Models/ProcedureAnimalAid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.Models
+{
+    public class ProcedureAnimalAid
+    {
+        [Required]
+        public int ProcedureId { get; set; }
+
+        [Required]
+        public virtual Procedure Procedure { get; set; }
+
+        [Required]
+        public int AnimalAidId { get; set; }
+
+        [Required]
+        public virtual AnimalAid AnimalAid { get; set; }
+    }
+}
