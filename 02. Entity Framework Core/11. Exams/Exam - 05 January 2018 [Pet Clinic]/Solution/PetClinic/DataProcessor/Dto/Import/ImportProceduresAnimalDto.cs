83e03ad3bb8850a785a8e558bb1f7d9125196582
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:44 +0300

    Create ImportProceduresAnimalDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Import/ImportProceduresAnimalDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PetClinic.DataProcessor.Dto.Import
+{
+    [XmlType("AnimalAid")]
+    public class ImportProceduresAnimalDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        //UNIQUE
+        public string Name { get; set; }
+    }
+}
