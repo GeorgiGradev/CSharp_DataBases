908f1aa4f1a0950b6fd4d343b5d212019d273929
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:53 +0300

    Create ImportAnimalPassportDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Import/ImportAnimalPassportDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.DataProcessor.Dto.Import
+{
+    public class ImportAnimalPassportDto
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{7}[0-9]{3}$")]
+        public string SerialNumber { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string OwnerName { get; set; }
+
+        [Required]
+        [RegularExpression(@"(^\+359[0-9]{9}$)|(^0[0-9]{9}$)")]
+        public string OwnerPhoneNumber { get; set; }
+
+        [Required]
+        public string RegistrationDate { get; set; }
+    }
+}
