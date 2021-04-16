901fa8b2c0b6f976a037ea243f40a8a2fb2f77ee
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:23 +0300

    Create Procedure.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Models/Procedure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace PetClinic.Models
+{
+    public class Procedure
+    {
+        public Procedure()
+        {
+            this.ProcedureAnimalAids = new HashSet<ProcedureAnimalAid>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public int AnimalId { get; set; }
+
+        [Required]
+        public Animal Animal { get; set; }
+
+        public int VetId { get; set; }
+
+        [Required]
+        public Vet Vet { get; set; }
+
+        public ICollection<ProcedureAnimalAid>ProcedureAnimalAids { get; set; }
+
+        [NotMapped]
+        public decimal Cost => this.ProcedureAnimalAids.Sum(paa => paa.AnimalAid.Price); // calculated by summing the price of the different services performed
+
+        public DateTime DateTime { get; set; }
+    }
+}
