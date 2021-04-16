f9f0d379f959344f7071279181460b2fcffdc645
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:37:06 +0300

    Create ExportAninalAidDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Export/ExportAninalAidDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PetClinic.DataProcessor.Dto.Export
+{
+    [XmlType("AnimalAid")]
+    public class ExportAninalAidDto
+    {
+        [XmlElement("Name")]
+        public string AidName { get; set; }
+
+        [XmlElement("Price")]
+        public decimal AidPrice { get; set; }
+    }
+}
