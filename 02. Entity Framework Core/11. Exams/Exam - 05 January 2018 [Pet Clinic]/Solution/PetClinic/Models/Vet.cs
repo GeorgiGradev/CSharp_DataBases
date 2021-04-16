a878ed06292c751d3bd9dc85ba99abccb26bda76
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:16 +0300

    Create Vet.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Models/Vet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.Models
+{
+    public class Vet
+    {
+        public Vet()
+        {
+            this.Procedures = new HashSet<Procedure>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string Profession { get; set; }
+
+        [Range(22, 65)]
+        public int Age { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[+]359[0-9$]{9}$|^0[0-9]{9}$")]
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Procedure> Procedures { get; set; }
+    }
+}
