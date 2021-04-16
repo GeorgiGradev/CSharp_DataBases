6ca15f1978bd3e8936eef6bbb936814396fc84bb
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:30 +0300

    Create AnimalAid.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/Models/AnimalAid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetClinic.Models
+{
+    public class AnimalAid
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<ProcedureAnimalAid> AnimalAidProcedures { get; set; }
+            = new HashSet<ProcedureAnimalAid>();
+    }
+}
