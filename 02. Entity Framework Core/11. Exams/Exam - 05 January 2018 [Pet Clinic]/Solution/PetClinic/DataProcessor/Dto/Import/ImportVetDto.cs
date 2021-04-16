ecf19eebb4f964d94c7a3839b032657ca08994f8
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-16 11:36:41 +0300

    Create ImportVetDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 05 January 2018 [Pet Clinic]/Solution/PetClinic/DataProcessor/Dto/Import/ImportVetDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PetClinic.DataProcessor.Dto.Import
+{
+    [XmlType("Vet")]
+    public class ImportVetDto
+    {
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string Profession { get; set; }
+
+        [Range(22, 65)]
+        public int Age { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\+359[0-9$]{9}$|^0[0-9]{9}$")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
