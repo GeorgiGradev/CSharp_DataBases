415ae8d5c4ce98701b3128d816b3f3813cf5da88
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:46:40 +0300

    Create Item.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.Models/Item.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFood.Models
+{
+    public class Item
+    {
+        public Item()
+        {
+            this.OrderItems = new HashSet<OrderItem>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public virtual Category Category { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+    }
+}
