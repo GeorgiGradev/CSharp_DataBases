2c3e59759411ff03767b8044b992694cc0a143bb
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:46:55 +0300

    Create Category.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Items = new HashSet<Item>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
