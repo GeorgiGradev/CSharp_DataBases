4930754c1d82cbd0ed2b9ccfc97329ab0947a04c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:47:06 +0300

    Create Serializer.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.DataProcessor/Serializer.cs
@@ -0,0 +1,53 @@
+﻿ using System;
+using System.IO;
+using System.Linq;
+using FastFood.Data;
+using FastFood.Models.Enums;
+using Newtonsoft.Json;
+
+namespace FastFood.DataProcessor
+{
+	public class Serializer
+	{
+        public static string ExportOrdersByEmployee(FastFoodDbContext context, string employeeName, string orderType)
+        {
+            var employees = context
+                .Employees
+                .Where(e => e.Name == employeeName)
+                .Select(e => new
+                {
+                    Name = e.Name,
+                    Orders = e.Orders
+                        .Where(o => o.Type.ToString() == orderType)
+                        .Select(o => new
+                        {
+                            Customer = o.Customer,
+                            Items = o.OrderItems
+                                .Select(oi => new
+                                {
+                                    Name = oi.Item.Name,
+                                    Price = decimal.Parse($"{oi.Item.Price:f2}"),
+                                    Quantity = oi.Quantity
+                                })
+                                .ToList(),
+                            TotalPrice = decimal.Parse($"{o.TotalPrice:f2}")
+                        })
+                        .OrderByDescending(o => o.TotalPrice)
+                        .ThenByDescending(o => o.Items.Count)
+                        .ToList(),
+                    TotalMade = e.Orders
+                        .Sum(o => decimal.Parse($"{o.TotalPrice:f2}"))
+                })
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(employees, Formatting.Indented);
+
+            return json;
+        }
+
+            public static string ExportCategoryStatistics(FastFoodDbContext context, string categoriesString)
+		{
+			return null;
+		}
+	}
+}
