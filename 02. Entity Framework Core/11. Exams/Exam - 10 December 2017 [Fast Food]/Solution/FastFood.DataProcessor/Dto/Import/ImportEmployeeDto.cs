ae72dc85d318fa292777f3be97d6a4070de0da08
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:47:29 +0300

    Create ImportEmployeeDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.DataProcessor/Dto/Import/ImportEmployeeDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FastFood.DataProcessor.Dto.Import
+{
+    public class ImportEmployeeDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Range(15, 80)]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Position { get; set; }
+    }
+}
