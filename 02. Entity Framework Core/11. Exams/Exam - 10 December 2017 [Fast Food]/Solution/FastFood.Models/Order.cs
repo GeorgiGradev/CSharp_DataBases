f56656885b47b5e94684d456048bceb025a51574
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:46:36 +0300

    Create Order.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using FastFood.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace FastFood.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            this.OrderItems = new HashSet<OrderItem>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Customer { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public OrderType Type { get; set; } = OrderType.ForHere;
+
+        [Required]
+        [NotMapped]
+        public decimal TotalPrice => this.OrderItems.Sum(x => x.Item.Price * x.Quantity);
+
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public virtual Employee Employee { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
