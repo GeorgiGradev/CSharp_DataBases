c1589da1e6ec2d382ba87db25a834c96aa3b342b
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:46:27 +0300

    Create Position.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.Models/Position.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models
+{
+    public class Position
+    {
+        public Position()
+        {
+            this.Employees = new HashSet<Employee>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
