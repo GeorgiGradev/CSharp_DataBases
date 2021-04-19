5c8c1d1c2e15f38fdb65f0ff583eb916e9f50ab6
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:47:22 +0300

    Create ImportItemDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.DataProcessor/Dto/Import/ImportItemDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FastFood.DataProcessor.Dto.Import
+{
+    public class ImportItemDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Category { get; set; }
+    }
+}
+ 
