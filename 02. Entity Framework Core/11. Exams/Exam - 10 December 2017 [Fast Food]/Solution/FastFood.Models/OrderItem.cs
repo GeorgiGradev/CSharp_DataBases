67627a4258bb2ae59d655cf80be9d66950d5e776
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:46:32 +0300

    Create OrderItem.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.Models/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFood.Models
+{
+    public class OrderItem
+    {
+        [ForeignKey(nameof(Order))]
+        public int OrderId { get; set; }
+
+        [Required]
+        public virtual Order Order { get; set; }
+
+        [ForeignKey(nameof(Item))]
+        public int ItemId { get; set; }
+
+        [Required]
+        public virtual Item Item { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
