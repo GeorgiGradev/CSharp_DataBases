3572f2a43118c48a81de7a29ff14cd109594c119
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:47:13 +0300

    Create ImportOrderItemDro.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.DataProcessor/Dto/Import/ImportOrderItemDro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace FastFood.DataProcessor.Dto.Import
+{
+    [XmlType("Item")]
+    public class ImportOrderItemDro
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [XmlElement("Quantity")]
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
