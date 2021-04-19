6143a04ee33db0ee6c5f0f3c2b97e90985b2f92c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-19 11:47:18 +0300

    Create ImportOrderDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 10 December 2017 [Fast Food]/Solution/FastFood.DataProcessor/Dto/Import/ImportOrderDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace FastFood.DataProcessor.Dto.Import
+{
+    [XmlType("Order")]
+    public class ImportOrderDto
+    {
+        [XmlElement("Customer")]
+        [Required]
+        public string Customer { get; set; }
+
+        [XmlElement("Employee")]
+        [Required]
+        public string Employee { get; set; }
+
+        [XmlElement("DateTime")]
+        public string DateTime { get; set; }
+
+        [XmlElement("Type")]
+        public string Type { get; set; } = "ForHere";
+
+        [XmlArray("Items")]
+        public List<ImportOrderItemDro> Items { get; set; }
+    }
+}
