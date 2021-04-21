28277c906d06df1a20507833a39affbd20940c0f
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:59 +0300

    Create Song.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,47 @@
+﻿using MusicHub.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+            this.SongPerformers = new HashSet<SongPerformer>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        [Required]
+        public int WriterId { get; set; }
+
+        [Required]
+        public virtual Writer Writer { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
