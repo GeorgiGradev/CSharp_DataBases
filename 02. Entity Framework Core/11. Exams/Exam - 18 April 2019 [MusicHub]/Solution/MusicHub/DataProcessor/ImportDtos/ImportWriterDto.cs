3e8444d72ebcfcc1fa3f836106f28120a4520d18
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:39 +0300

    Create ImportWriterDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/DataProcessor/ImportDtos/ImportWriterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    public class ImportWriterDto
+    {
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+ [A-Z][a-z]+$")]
+        public string Pseudonym { get; set; }
+    }
+}
