a4ae5525838e8fde2e904f760b35a702982716c1
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:30 +0300

    Create ImportProducerDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/DataProcessor/ImportDtos/ImportProducerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    public class ImportProducerDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+ [A-Z][a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression(@"^\+359 [0-9]{3} [0-9]{3} [0-9]{3}$")]
+        public string PhoneNumber { get; set; }
+
+        public List<ImportAlbumDto> Albums { get; set; }
+    }
+}
