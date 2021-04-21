fe708a0396a6fd95afff130fd2411447213bbe6b
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:36:02 +0300

    Create Producer.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            this.Albums = new HashSet<Album>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+ [A-Z][a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression(@"^\+359 [0-9]{3} [0-9]{3} [0-9]{3}$")]
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
+
