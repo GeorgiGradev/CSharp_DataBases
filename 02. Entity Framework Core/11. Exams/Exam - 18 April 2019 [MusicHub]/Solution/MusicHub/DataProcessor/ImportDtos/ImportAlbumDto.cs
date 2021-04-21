64cdfafb9a23b44f757d748d26a4a74771ad6235
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:21 +0300

    Create ImportAlbumDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/DataProcessor/ImportDtos/ImportAlbumDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    public class ImportAlbumDto
+    {
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string ReleaseDate { get; set; }
+    }
+}
