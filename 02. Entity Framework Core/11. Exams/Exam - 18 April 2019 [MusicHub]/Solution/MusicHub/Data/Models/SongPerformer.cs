09509cbd88a3e78785245f6c9fc8f6136fcf9ef8
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:55 +0300

    Create SongPerformer.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/Data/Models/SongPerformer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class SongPerformer
+    {
+        public int SongId { get; set; }
+
+        [Required]
+        public virtual Song Song { get; set; }
+
+        public int PerformerId  { get; set; }
+
+        [Required]
+        public virtual Performer Performer { get; set; }
+    }
+}
