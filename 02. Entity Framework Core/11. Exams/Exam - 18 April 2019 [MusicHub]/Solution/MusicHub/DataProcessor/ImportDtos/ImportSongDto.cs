5f63622cdbca53b70c236d8bd38256e58b75575d
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:35 +0300

    Create ImportSongDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/DataProcessor/ImportDtos/ImportSongDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Song")]
+    public class ImportSongDto
+    {
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Duration { get; set; }
+
+        [Required]
+        public string CreatedOn { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        public int? AlbumId { get; set; }
+
+        [Required]
+        public int WriterId { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+    }
+}
