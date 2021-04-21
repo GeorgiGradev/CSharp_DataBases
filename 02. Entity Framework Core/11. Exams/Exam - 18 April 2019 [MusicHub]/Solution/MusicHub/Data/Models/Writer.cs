900d51f855bbdac2eb7fadd523961306c5c5f1c5
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:51 +0300

    Create Writer.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Writer
+    {
+        public Writer()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+ [A-Z][a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
