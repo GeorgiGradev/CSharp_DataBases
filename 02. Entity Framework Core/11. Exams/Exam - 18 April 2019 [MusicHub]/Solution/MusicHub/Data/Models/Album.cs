aa95e8ab6ae31fd71b97bbfb65e50b3e975e48d5
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:36:15 +0300

    Create Album.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => Songs.Sum(x => x.Price);
+
+        public int ProducerId { get; set; }
+
+        public virtual Producer Producer { get; set; } 
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
