3409c25b60942376c733b84d7b7e6fa001d9c4cb
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-04-21 08:35:27 +0300

    Create ImportPerformerSongDto.cs

---
+++ 02. Entity Framework Core/11. Exams/Exam - 18 April 2019 [MusicHub]/Solution/MusicHub/DataProcessor/ImportDtos/ImportPerformerSongDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Song")]
+    public class ImportPerformerSongDto
+    {
+        [XmlAttribute("id")]
+        public int SongId { get; set; }
+    }
+}
